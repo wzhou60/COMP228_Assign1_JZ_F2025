1b58d3d8a17bc4655d2184f0e838a13f10a3eae7
dillcheese <dillcheese1@gmail.com>
2025-09-08 11:44:38 -0400

    switch

---
+++ src/com/va/week1/assign1/DriverMain.java
@@ -0,0 +1,17 @@
+package com.va.week1.assign1;
+
+public class DriverMain {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Create objects for classes of task a-d
+		 * 
+		 */
+
+		GradeCalculation gc = new GradeCalculation();
+		
+		gc.GradeCalc();
+	}
+
+}
